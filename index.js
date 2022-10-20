83c284c3417533b629be0036e4383d2744aefbb6
Sara Youssef <sarary2000@gmail.com>
2022-10-21 01:06:03 +0400

    Done

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Your code goes here
+
+const Para = document.querySelector('p');
+Para.textContent= "JavaScript is so cool. It lets me add text to my page programmatically.";
+const event = document.createEvent( 'Event' );
+
+document.addEventListener("DOMContentLoaded", function() {
+    //console.log("The DOM has loaded");
+    const texts=document.getElementById("text");
+    texts.textContent="This is really cool!" 
+  });
+  
